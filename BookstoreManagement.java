f5abaf8218399857567b1658878ccca95c2d1fbe
Rohit-Yadav07 <89621826+Rohit-Yadav07@users.noreply.github.com>
2023-03-10 21:48:28 +0530

    Update BookstoreManagement.java

--- BookstoreManagement.java
+++ BookstoreManagement.java
@@ -1,1 +1,10 @@
 import java.util.Scanner;
+public class BookstoreManagement {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] books = {"Book 1", "Book 2", "Book 3", "Book 4", "Book 5"};
+        double[] prices = {10.99, 12.99, 9.99, 15.99, 8.99};
+        int[] quantities = {10, 15, 20, 5, 25};
+        double totalRevenue = 0.0;
